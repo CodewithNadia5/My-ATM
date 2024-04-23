0b28a7906f35a19f3d779d2dd44fdd5f51fcd6e7
CodewithNadia5 <nadianoreen720@gmail.com>
2024-04-24 04:52:50 +0500

    Adding a cashcraft ATM Machine

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-#!/usr/bin/env node
+#! /usr/bin/env node
 import inquirer from "inquirer";
 let myBalance = 10000; //Dollar
 let myPin = 1234;
+// Print Welcome message
+console.log("Welcome to My ATM Machine");
 let pinAnswer = await inquirer.prompt([
     {
         name: "pin",
@@ -16,7 +18,7 @@ if (pinAnswer.pin === myPin) {
             name: "operation",
             message: "Please select option",
             type: "list",
-            choices: ["withdraw", "check balance"]
+            choices: ["withdraw", "fast cash", "check balance"]
         }
     ]);
     if (operationAns.operation === "withdraw") {
@@ -27,9 +29,26 @@ if (pinAnswer.pin === myPin) {
                 type: "number"
             }
         ]);
-        //  = += -=
-        myBalance -= amountAns.amount;
-        console.log("your remaining balance is " + myBalance);
+        if (amountAns.amount > myBalance) {
+            console.log("Insufficient Balance");
+        }
+        // = += -+
+        else {
+            myBalance -= amountAns.amount;
+            console.log(`Your Remaining Balance is: ${myBalance}`);
+        }
+    }
+    else if (operationAns.operation === "fast cash") {
+        let fast = await inquirer.prompt([
+            {
+                name: "fastcash",
+                message: "Select the amount which you withdrawal",
+                type: "list",
+                choices: [1000, 2000, 5000, 10000]
+            }
+        ]);
+        myBalance -= fast.fastcash;
+        console.log(`You have successfully withdrawal ${fast.fastcash} \nYour remaining balance ${myBalance}`);
     }
     else if (operationAns.operation === "check balance") {
         console.log(`your remaining balance is ${myBalance}`);
